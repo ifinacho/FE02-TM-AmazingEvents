965d266d1d9f82d0ac62b5d7340275f57f2d65de
Ignacio Francisco Ibañez <endermindkai@gmail.com>
2023-02-24 16:20:17 -0300

    version11

--- data.js
+++ data.js
@@ -171,3 +171,19 @@ let data = {
       }
     ]
   };
+
+
+function createCard(event){
+  let card = `<div class="card" style="width: 18rem;">
+  <img src="${event.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+      <h5 class="card-title">${event.name}</h5>
+      <p class="card-text">${event.description}</p>
+      <div class="divcardbody">
+          <span>Price ${event.price}</span>
+          <a href="./details.html" class="btn btn-primary">ver más...</a>
+      </div>
+  </div>
+</div>`
+  return card
+}
